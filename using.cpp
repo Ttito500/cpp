b0b8bd17ebc8d4214f5a55a58c4dcd87966c4321
Tiago <tiago.tito.sampaio@gmail.com>
2024-05-09 13:53:30 -0300

    typedef/using

--- using.cpp
+++ using.cpp
@@ -1,1 +1,13 @@
 //typedef std::string text
+#include <iostream>
+
+using txt = std::string;
+using num = int;
+
+int main(){
+    txt name = "tito";
+    num age = 12;
+
+    std::cout << name << " " << age << '\n';
+
+}
