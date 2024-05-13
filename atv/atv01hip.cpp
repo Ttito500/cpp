d7ba12cda273b185673efe762226faf631268744
Tiago <tiago.tito.sampaio@gmail.com>
2024-05-13 18:03:48 -0300

    atv1

---
+++ atv/atv01hip.cpp
@@ -0,0 +1,17 @@
+//calcular hipotenusa
+
+#include <iostream>
+#include <cmath>
+
+int main(){
+    float cat1, cat2;
+
+    std::cout << "Whats cat1? ";
+    std::cin >> cat1;
+    std::cout << "Whats cat2? ";
+    std::cin >> cat2;
+
+    std::cout << "Hip is " << sqrt((cat1, 2) + pow(cat2, 2));
+
+    return 0;
+}
