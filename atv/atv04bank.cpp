5748d7fa6b5ba414550bef0a7d40fd47627335c8
Tiago <tiago.tito.sampaio@gmail.com>
2024-05-24 18:54:45 -0300

    finished atv4

--- atv/atv04bank.cpp
+++ atv/atv04bank.cpp
@@ -2,37 +2,48 @@
 #include <iostream>
 float balance; //if i was using oop i wouldnt do this
 
-float withdraw(float value);
-float deposit(float value);
-std::string show(float bal);
+void withdraw(float value);
+void deposit(float value);
 
 int main(){
-
-    std::cout << "d for deposit, w for withdraw, b to show balance\n";
-    char op;
-    std::cin >> op;
-    
-    switch (op)
+    do
     {
-    case d:
-        /* code */
-        break;
-    
-    default:
-        break;
-    }
+        std::cout << "d for deposit, w for withdraw, b to show balance\n";
+        char op;
+        std::cin >> op;
+
+        float val;
+        switch (op)
+        {
+        case 'd':
+            val = 0;
+            std::cout << "how much will u deposit?\n";
+            std::cin >> val;
+            deposit(val);
+            break;
+        case 'w':
+            val = 0;
+            std::cout << "how much will u withdraw?\n";
+            std::cin >> val;
+            withdraw(val);
+            break;
+        case 'b':
+            std::cout << "You have " << balance << " in ur balance!\n";
+            break;
+        default:
+            std::cout << "please, enter a valid input\n";
+            break;
+        }
+    } while (balance >= 0);
 
     return 0;
 }
 
-float withdraw(float value){
+void withdraw(float value){
     balance =- value;
 }
 
-float deposit(float value){
+void deposit(float value){
     balance =+ value;
 }
 
-std::string show(float bal){
-    return "your balance is " + bal;
-}
