0cdb103a45838ffb1cd422da3c1d2fefd7d3b006
Tiago <tiago.tito.sampaio@gmail.com>
2024-05-24 14:35:01 -0300

    atv4

--- atv/atv04bank.cpp
+++ atv/atv04bank.cpp
@@ -1,10 +1,38 @@
 //deposit, withdraw and show
 #include <iostream>
+float balance; //if i was using oop i wouldnt do this
 
-
+float withdraw(float value);
+float deposit(float value);
+std::string show(float bal);
 
 int main(){
-    float balance;
+
+    std::cout << "d for deposit, w for withdraw, b to show balance\n";
+    char op;
+    std::cin >> op;
+    
+    switch (op)
+    {
+    case d:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
 
     return 0;
+}
+
+float withdraw(float value){
+    balance =- value;
+}
+
+float deposit(float value){
+    balance =+ value;
+}
+
+std::string show(float bal){
+    return "your balance is " + bal;
 }
