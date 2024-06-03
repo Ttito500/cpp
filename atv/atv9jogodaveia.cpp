112dcbb712254e404a9cfacb7e5e6e7c3556be44
Tiago <tiago.tito.sampaio@gmail.com>
2024-06-03 17:37:47 -0300

    basics

--- atv/atv9jogodaveia.cpp
+++ atv/atv9jogodaveia.cpp
@@ -34,23 +34,61 @@ int main(){
             int x, y;
             x = playerMove.at(0) - '0';
             y = playerMove.at(2) - '0';
+
+            play(matrix, 'x', x, y);
         }else
         {
             cpuplay(matrix);
         }
         
+        showgame(matrix);
+        i++;
     }
     
     return 0;
 }
 
 void play(char (&matrix)[3][3], char xo, int x, int y){
-    if (matrix[y][x] == 'e')
+    if (matrix[x][y] == 'e')
     {
-        matrix[y][x] = xo;
+        matrix[x][y] = xo;
 
     }else
     {
         std::cout << "faz o L\n";
+    }
+}
+
+void cpuplay(char (&matrix)[3][3]){
+    bool played = false;
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            if(matrix[i][j] == 'e')
+            {
+                matrix[i][j] = 'o';
+                played = true;
+                break;
+            }
+        }
+        if (played)
+        {
+            break;
+        }
+    }    
+}
+
+char checkvictory(char (&matrix)[3][3]);
+
+void showgame(char (&matrix)[3][3]){
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            std::cout << matrix[i][j] << ' ';
+        }
+        std::cout << '\n';
     }
 }
